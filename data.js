fdc966bd11cbe03582c3c418a699d98975d778c9
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-06 12:21:55 +0530

    plus

--- data.js
+++ data.js
@@ -3,3 +3,20 @@ var num1=120;
 var num2=987;
 
 console.log ( "sum of num1 and num2="+ (num1+num2))
+
+
+function okk(){
+
+   let ans= window.prompt("Enter Your Num1")
+
+
+   let as= window.prompt("Enter Your Num2")
+
+
+//    window.alert(parseInt(ans)+parseInt(as))
+      window.alert(parseInt(ans)+(as))
+
+
+}
+
+okk()
