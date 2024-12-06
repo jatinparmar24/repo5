985c392416a7c52fa8e5fe522728befe11a714d9
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-06 12:35:00 +0530

    confirm

--- data.js
+++ data.js
@@ -7,14 +7,26 @@ console.log ( "sum of num1 and num2="+ (num1+num2))
 
 function okk(){
 
-   let ans= window.prompt("Enter Your Num1")
+  // let ans= window.prompt("Enter Your Num1")
 
 
-   let as= window.prompt("Enter Your Num2")
+   //let as= window.prompt("Enter Your Num2")
 
 
 //    window.alert(parseInt(ans)+parseInt(as))
-      window.alert(parseInt(ans)+(as))
+    //   window.alert(parseInt(ans)+(as))
+    // window.alert(parseInt(ans-as))
+   // window.alert(parseInt(ans*as))
+   // window.alert(parseInt(ans/as))
+
+  let as1 = window.confirm("Are U happy with this website")
+
+  if(as1==true){
+    window.alert("happy")
+  }
+  else{
+    window.alert("bad")
+  }
 
 
 }
