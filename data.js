1e556e84e7aeb595f00e39494052d0b8d96d5e40
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-07 12:34:58 +0530

    even odd

--- data.js
+++ data.js
@@ -1,34 +1,113 @@
 
-var num1=120;
-var num2=987;
+// var num1=120;
+// var num2=987;
 
-console.log ( "sum of num1 and num2="+ (num1+num2))
-
-
-function okk(){
-
-  // let ans= window.prompt("Enter Your Num1")
+// console.log ( "sum of num1 and num2="+ (num1+num2))
 
 
-   //let as= window.prompt("Enter Your Num2")
+// function okk(){
+
+//   // let ans= window.prompt("Enter Your Num1")
 
 
-//    window.alert(parseInt(ans)+parseInt(as))
-    //   window.alert(parseInt(ans)+(as))
-    // window.alert(parseInt(ans-as))
-   // window.alert(parseInt(ans*as))
-   // window.alert(parseInt(ans/as))
+//    //let as= window.prompt("Enter Your Num2")
 
-  let as1 = window.confirm("Are U happy with this website")
 
-  if(as1==true){
-    window.alert("happy")
+// //    window.alert(parseInt(ans)+parseInt(as))
+//     //   window.alert(parseInt(ans)+(as))
+//     // window.alert(parseInt(ans-as))
+//    // window.alert(parseInt(ans*as))
+//    // window.alert(parseInt(ans/as))
+
+//   let as1 = window.confirm("Are U happy with this website")
+
+//   if(as1==true){
+//     window.alert("happy")
+//   }
+//   else{
+//     window.alert("bad")
+//   }
+
+
+// }
+
+// okk()
+
+// function even(){
+
+//   let a=4;
+//   if(a%2==0){
+//     alert("even")
+//   }
+//   else{
+//     alert("odd")
+//   }
+// }
+
+// even()
+
+function numbers(){
+
+//   let num=-9;
+//   if(num<0){
+//     alert("negative")
+//   }
+//   else if(num==0){
+//     alert("zero")
+//   }
+//   else{
+//     alert("positive")
+//   }
+
+  // ternary operator;
+  // conditon ? "true":"false"
+
+  // let age=18;
+  // let ans = age>18 ? "younger":"elder";
+  // alert(ans)
+
+
+  // let num;
+
+  // for(num=1; num<=10; num++)
+
+  //   console.log(num)
+
+  // let num;
+  // for(num=1; num<=10; num++)
+  //   console.log(2*num)
+
+
+
+  // let ans = prompt("enter your age")
+
+  // if(ans>=18){
+  //   alert("You can vote")
+  // }
+  // else{
+  //   alert("you can't vote")
+  // }
+
+  // let ans= prompt("Enter Your Age")
+
+  // ans>=18?alert("Adult"):alert("Child")
+
+  // let ans= confirm("Have u complete your course")
+  // let ans2= confirm("have u got the certificate")
+  // alert(ans && ans2)
+
+  let ans= confirm("Have u complete your course")
+  let ans2= confirm("have u got the certificate")
+
+  if(ans==true && ans2== true){
+    alert("completed")
   }
   else{
-    window.alert("bad")
+    alert(" not completed")
   }
 
 
-}
 
-okk()
+ }
+
+numbers()
