cc46cb26c65ea1a59e2bb1b94dd34ed0f080141c
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-08 23:13:23 +0530

    not happ

--- data.js
+++ data.js
@@ -148,10 +148,65 @@ function numbers(){
 
  //Q-8
 
- let num=1;
+ //let num;
 
- while(num<=10);
- console.log("num")
+// while( num<=10);
+ //{ 
+ // console.log(num)
+ // num++;
+ //}
+
+  //Q-9
+
+ let num;
+
+ //do{
+ //  prompt("Enter Number")
+   
+ //}
+// while(num>=10);{
+// alert("Greater")
+// }
+
+ //Q-10 with if else
+
+ //let day=prompt("Enter 1 to 7")
+ //if(day==1){
+ // alert("Monday")
+ //}
+ //else if(day==2){
+ // alert("Tuesday")
+ //}
+ //else if(day==3){
+ // alert("Wednesday")
+ //}
+ //else if(day==4){
+ // alert("Tharsday")
+ //}
+ //else if(day==5){
+ // alert("Friday")
+ //}
+ //else if(day==6){
+ // alert("Saturday")
+ //}
+// else if(day==7){
+ // alert("Sunday")
+ //}
+ //else{
+ // alert("Invalid day")
+ //}
+ 
+//Q-10 with switch case
+
+let day= prompt("Enter Between 1 to 7")
+switch(day==1){
+  case 0 : alert("Monday");
+}
+switch(day==2){
+  case 1 : alert ("Tuesday");
+}
+
+
 
 
 }
