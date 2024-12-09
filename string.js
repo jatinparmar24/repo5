9ef62d471cf11dee5f352f1871e922d34189782f
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-09 12:33:51 +0530

    were

--- string.js
+++ string.js
@@ -1,3 +1,32 @@
   function okk(){
-    alert("Hello World")
+   // alert("Hello World")
+
+  // let text= "This is String"
+
+   //alert(text)
+  // alert(text.length)
+  // document.write(text.length) to print on body
+  //document.write(text.slice(2,9)) last digit is minus 1 always in slice
+
+
+
+
+
+  
+   
+ // let text= "This"  index value starts from zero
+  // document.write(text.charAt(2))  (2) it is index
+
+
+  //let text= "This is an Cybrom"
+ // let text2= "Okk"
+ // document.write(text.concat(" "+text2)) " "+ to add additional space
+
+
+ let text= "   this    "
+ //document.write(text.trim().length) one mwthod
+
+
+
+
   }
