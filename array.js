0288d7c7c9a78786898f88b1da66cd812efe0a8d
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-11 12:37:18 +0530

    color

--- array.js
+++ array.js
@@ -35,9 +35,11 @@ function okk(){
 
 
 
+
+
  // short method gives data in accending order
 
- let arr= [4,7,3,9,2,6,1]
+ //let arr= [4,7,3,9,2,6,1]
  //let ans =arr.sort()
  //document.write(ans)
 
@@ -45,12 +47,30 @@ function okk(){
  //let ans= arr.reverse()
  //document.write(ans)
 
- 
+
 
  //decending order
 
  //let ans= arr.sort().reverse()
  //document.write(ans)
 
+// let ar1= [1,3,5,7,9]
+ // let ar2= [2,4,6,8]
+
+ //let ans= ar1.concat(ar2)
+// ans.sort()
+ //document.write(ans)
+
  
+}
+
+//pick id in js
+
+function start(){
+
+    let show= document.getElementById("text")
+    show.textContent=("Yes it comes")
+    show.style.color="blue"
+
+
 }
