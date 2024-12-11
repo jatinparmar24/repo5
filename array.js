4029cd74f53cd5da51e4511471943f3b8f1f8262
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-11 12:10:08 +0530

    array

---
+++ array.js
@@ -0,0 +1,36 @@
+// it store multiple data types or same data types or variables or number etc
+
+function okk(){
+
+    let fruits=["Apple","Mango","Grapes","Banana"]
+
+   // let ans=fruits.length;
+   // document.write(ans)
+
+
+  //pop removes last element of an array
+   //let ans=fruits.pop()
+   // have to use pop again to remove second element
+ //  document.write(ans) it stores pop means last one which it removes
+ //  document.write(fruits) it store what reamin after pop
+
+ // shift it removes starting element
+
+ //let ans= fruits.shift()
+ //document.write(ans) gives index number
+ //document.write(fruits) adds new one
+
+ //push adds the last element
+
+ //let ans=fruits.push("Orange","Avocadra")
+ //document.write(ans)
+ //document.write(fruits)
+
+ // unshift adds an element in starting
+
+// let ans= fruits.unshift("Oranges") 
+// document.write(ans)
+ //document.write(fruits)
+
+ 
+}
