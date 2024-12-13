a34f146bf4b10b207332797f9315dc1bf56ff7a1
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-13 22:07:15 +0530

    box

---
+++ try.js
@@ -0,0 +1,22 @@
+
+
+let red=()=>{
+
+    let des= document.querySelector("#box")
+    des.style.backgroundColor="red"
+    des.style.borderRadius="0%"
+}
+
+let blue=()=>{
+
+    let des= document.querySelector("#box")
+    des.style.backgroundColor="blue"
+    des.style.borderRadius="100%"
+}
+
+let yellow=()=>{
+
+    let des= document.querySelector("#box")
+    des.style.backgroundColor="yellow"
+    des.style.borderRadius="90px"
+}
