eb5eedaf61215a481f45e84e6c14a5e29fa8bc39
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-14 12:39:20 +0530

    porting of

---
+++ ques.js
@@ -0,0 +1,42 @@
+ let= ques=() =>{
+ // q-1
+
+  // let name = prompt("Enter Your Name");
+  //  alert("Hello," + name +"!")
+
+    // q-1
+
+    // q-2
+
+  //  let name = confirm("Do You Like Using Javascript");
+  //  if(name==true){
+  //      alert("That's Great")
+  //  }
+   // else{
+   //     alert("No Worries,Keep Learning")
+   // }
+
+   // q-2
+
+   // q-3
+
+   //  let str1= "Javascript is easy, i love javascript"
+   //  let con= confirm(str1)
+   //  if(con==true){
+   //     let rev=str1.replace(/Javascript/g, "coding")
+   //     alert(rev)
+   //  }
+   //  else{
+   //     alert(str1)
+   //  }
+
+    // q-3
+
+    // q-4
+
+
+
+
+
+ }
+ ques()
