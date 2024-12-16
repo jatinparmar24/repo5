7955c540597a89b050b2f3d7c0501f7e5c62117b
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-16 12:28:41 +0530

    adding things

--- object.js
+++ object.js
@@ -7,7 +7,29 @@ let obj= {         // has unique key value
      age:23,
      city:"Sehore",
 
+     // we can make function also in object
+
+     welcome: function(){
+        console.log("Welcome to my Home Page")
+     }
+
 }
 
 console.log(arr[1])  // gives index value
 console.log(obj.name);  // object access
+
+// to add another thing in object
+
+obj.email="Jatin232423@gmail.com";
+console.log(obj.email)
+console.log(obj)          // update after email add
+
+
+ // to delete
+
+ delete obj.city;    // city has been deleted
+
+ // to see function or to call it
+
+ obj.welcome();
+
