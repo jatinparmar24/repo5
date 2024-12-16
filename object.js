4607aca79b84a31ba656464507dbae3785f1b68f
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-16 12:08:35 +0530

    object

---
+++ object.js
@@ -0,0 +1,13 @@
+let arr= ["Jatin",23,"Sehore"] // its array
+let str= "kjalkhdgku,aljkdh.,kahfsdjh";
+
+let obj= {         // has unique key value
+
+     name:"Jatin",
+     age:23,
+     city:"Sehore",
+
+}
+
+console.log(arr[1])  // gives index value
+console.log(obj.name);  // object access
