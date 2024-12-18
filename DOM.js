63860189c13b39b44ff87eaccf965e3122e78e0c
jatinparmar24 <jatinparmar2424@gmail.com>
2024-12-18 19:30:19 +0530

    draw

---
+++ DOM.js
@@ -0,0 +1,16 @@
+let go=()=>{
+
+    let show=document.querySelector("#dis")
+    show.innerHTML="Hello Javascript";
+    show.style.color="red";
+    show.style.fontSize="40px";
+}
+
+let add=()=>{
+    let show=document.querySelector("#in")
+    show.innerHTML=1;
+}
+let sub=()=>{
+    let show=document.querySelector("#in")
+    show.innerHTML=-1;
+}
