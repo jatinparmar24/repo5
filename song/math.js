c85c7d5eab5b519e23b43cc3deca3a2ddc41702f
jatinparmar24 <jatinparmar2424@gmail.com>
2025-01-03 20:29:26 +0530

    whym

--- song/math.js
+++ song/math.js
@@ -1,7 +1,7 @@
  // console.log(Math.PI);
  // console.log(Math.abs(-8));   // gives positive number of negetive
 
- console.log(Math.floor(Math.random()+1));      // floor removes digit after point(.)   random gives digit between 0 and 1
+ console.log(Math.floor(Math.random()+2));      // floor removes digit after point(.)   random gives digit between 0 and 1
  
  
   // dice function
@@ -9,9 +9,17 @@
   let roll=()=>{
 
    let show=document.querySelector("#mid")
+   let show1=document.querySelector("#mid2")
+   let show2=document.querySelector("#mid3")
 
    if(Math.floor(Math.random()+1)){
     show.style.backgroundColor="red"
+   }
+
+  
+  else if(Math.floor(Math.random()+2)){
+    show1.style.backgroundColor="red"
+    show2.style.backgroundColor="red"
    }
      
 
