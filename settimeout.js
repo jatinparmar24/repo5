39155642dfa6cb0a2091a6c42aefc551b2b0c48e
jatinparmar24 <jatinparmar2424@gmail.com>
2025-01-03 12:46:24 +0530

    almost done alarm

--- settimeout.js
+++ settimeout.js
@@ -195,23 +195,43 @@
      show.innerHTML=currenttime
    },1000)
 
-   // alarm
+   // alarm clock
 
    let rune=()=>{
 
-    let show1=document.querySelector("#inp").value
+    let show1=document.querySelector("#inp").value;
+   
 
-    setInterval(()=>{
+   let inter= setInterval(()=>{
         
     let war=document.querySelector("#shows")
+     war.innerHTML=`Alarm Set For ${show1}`
 
-    let time=new Date()
-      time=`${time.getHours()}:${time.getMinutes()}`
+    let time=new Date()   // time in object form=====
 
-    if(show1==time)
-    war.innerHTML="Alarm Snoozing"; 
+      time=`${time.getHours().toString() .padStart(2,"0")}:${time.getMinutes().toString() .padStart(2,"0")}`   // get hours are in  number form so we use to string to change intt string because padstart take value in strinf format ===== padstart take 2 parameter first is how many digit u want to add and secondly whar u want to add
+
+    if(show1==time){
+    war.innerHTML="Alarm Snoozing";
+     document.querySelector("#sing").play()
     war.style.color="red"
     war.style.fontSize="50px"
-    },1000)
 
-   }
+    setTimeout(() => {
+      document.querySelector("#sing").pause()
+      clearInterval(inter)
+    }, 6000);
+    }
+
+    else{
+      war.innerHTML="Alarm Set";
+    }
+  },1000)
+
+   
+
+
+  }
+
+
+  
