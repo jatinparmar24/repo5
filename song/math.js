e43da17001313e577e4a2f9bf7e6992e18fcb13e
jatinparmar24 <jatinparmar2424@gmail.com>
2025-01-04 12:32:35 +0530

    roll com

--- song/math.js
+++ song/math.js
@@ -1,16 +1,77 @@
  // console.log(Math.PI);
  // console.log(Math.abs(-8));   // gives positive number of negetive
 
- console.log(Math.floor(Math.random()*6))+2;      // floor removes digit after point(.)   random gives digit between 0 and 1
+ console.log(Math.floor(1000+Math.random()*9000));      // floor removes digit after point(.)   random gives digit between 0 and 1
+ console.log(Math.floor(Math.random()*6)+1);
  
  
   // dice function
 
   let roll=()=>{
 
-    let result=document.querySelector("#result") 
-       let numb=Math.floor(Math.random()*6)+1;
-       result.innerHTML=numb;
+     let dice=(Math.floor(Math.random()*6)+1)
+
+     let box1=document.querySelector("#box1")
+     let box2=document.querySelector("#box2")
+     let box3=document.querySelector("#box3")
+     let box4=document.querySelector("#box4")
+     let box5=document.querySelector("#box5")
+     let box6=document.querySelector("#box6")
+
+     if(dice==1){
+      box1.style.opacity=1;
+      box2.style.opacity=0;
+      box3.style.opacity=0;
+      box4.style.opacity=0;
+      box5.style.opacity=0;
+      box6.style.opacity=0;
+     }
+
+     else if(dice==2){
+      box1.style.opacity=0;
+      box2.style.opacity=1;
+      box3.style.opacity=0;
+      box4.style.opacity=0;
+      box5.style.opacity=0;
+      box6.style.opacity=0;
+     }
+
+     else if(dice==3){
+      box1.style.opacity=0;
+      box2.style.opacity=0;
+      box3.style.opacity=1;
+      box4.style.opacity=0;
+      box5.style.opacity=0;
+      box6.style.opacity=0;
+     }
+
+     else if(dice==4){
+      box1.style.opacity=0;
+      box2.style.opacity=0;
+      box3.style.opacity=0;
+      box4.style.opacity=1;
+      box5.style.opacity=0;
+      box6.style.opacity=0;
+     }
+
+     else if(dice==5){
+      box1.style.opacity=0;
+      box2.style.opacity=0;
+      box3.style.opacity=0;
+      box4.style.opacity=0;
+      box5.style.opacity=1;
+      box6.style.opacity=0;
+     }
+
+     else if(dice==6){
+      box1.style.opacity=0;
+      box2.style.opacity=0;
+      box3.style.opacity=0;
+      box4.style.opacity=0;
+      box5.style.opacity=0;
+      box6.style.opacity=1;
+     }
+  
 
 
 
