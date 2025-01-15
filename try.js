a1006202be45c57cd3182639c7661aae3073b5cf
jatinparmar24 <jatinparmar2424@gmail.com>
2025-01-15 19:23:47 +0530

    json start

--- try.js
+++ try.js
@@ -1,70 +1,70 @@
 
 
-// let red=()=>{
+let red=()=>{
 
-//     let des= document.querySelector("#box")
-//     des.style.backgroundColor="red"
-//     des.style.borderRadius="0%"
-// }
+    let des= document.querySelector("#box")
+    des.style.backgroundColor="red"
+    des.style.borderRadius="0%"
+}
 
-// let blue=()=>{
+let blue=()=>{
 
-//     let des= document.querySelector("#box")
-//     des.style.backgroundColor="blue"
-//     des.style.borderRadius="100%"
-// }
+    let des= document.querySelector("#box")
+    des.style.backgroundColor="blue"
+    des.style.borderRadius="100%"
+}
 
-// let yellow=()=>{
+let yellow=()=>{
 
-//     let des= document.querySelector("#box")
-//     des.style.backgroundColor="yellow"
-//     des.style.borderRadius="90px"
-// }
+    let des= document.querySelector("#box")
+    des.style.backgroundColor="yellow"
+    des.style.borderRadius="90px"
+}
 
-// let green=()=>{
+let green=()=>{
 
-//     let des= document.querySelector("#box1")
-//     des.style.backgroundColor="green"
-//     des.style.borderRadius="0%"
-// }
+    let des= document.querySelector("#box1")
+    des.style.backgroundColor="green"
+    des.style.borderRadius="0%"
+}
 
-// let aqua=()=>{
+let aqua=()=>{
 
-//     let des= document.querySelector("#box1")
-//     des.style.backgroundColor="aqua"
-//     des.style.borderRadius="100%"
-// }
+    let des= document.querySelector("#box1")
+    des.style.backgroundColor="aqua"
+    des.style.borderRadius="100%"
+}
 
-// let purple=()=>{
+let purple=()=>{
 
-//     let des= document.querySelector("#box1")
-//     des.style.backgroundColor="purple"
-//     des.style.borderRadius="90px"
-// }
+    let des= document.querySelector("#box1")
+    des.style.backgroundColor="purple"
+    des.style.borderRadius="90px"
+}
 
-// let first=()=>{
+let first=()=>{
     
   
-//     let des= document.querySelector("#image")
-//     des.style.backgroundImage="url('image/dest1.jpg')";
-//     des.style.backgroundSize="contain";
-// }
+    let des= document.querySelector("#image")
+    des.style.backgroundImage="url('image/dest1.jpg')";
+    des.style.backgroundSize="contain";
+}
 
-// let first1=()=>{
-//     let des= document.querySelector("#image")
-//     des.src="url('image/dest3.jpg')";
-// }
+let first1=()=>{
+    let des= document.querySelector("#image")
+    des.src="url('image/dest3.jpg')";
+}
 
-// let second=()=>{
-//     let des= document.querySelector("#image")
-//     des.style.backgroundImage="url(image/dest2.jpg)"
-// }
+let second=()=>{
+    let des= document.querySelector("#image")
+    des.style.backgroundImage="url(image/dest2.jpg)"
+}
 
-// let third=()=>{
-//     let des= document.querySelector("#image")
-//     des.style.backgroundImage="url(image/dest3.jpg)"
+let third=()=>{
+    let des= document.querySelector("#image")
+    des.style.backgroundImage="url(image/dest3.jpg)"
 
-// }
+}
 
 
 
